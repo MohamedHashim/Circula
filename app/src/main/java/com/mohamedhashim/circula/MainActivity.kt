3c0d0de98bb16276c0c6b80591e91095f2126e11
Mohamed Hashim <mohamedhashim28@gmail.com>
2021-02-20 23:57:54 +0200

    Initial commit

---
+++ app/src/main/java/com/mohamedhashim/circula/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mohamedhashim.circula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/**
+ * Created by Mohamed Hashim on 2/20/2021.
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
